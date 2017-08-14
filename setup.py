6c06570cc80c533352689e2df013fa8a4033ab54
Sergey Punkoff <svpmailbox@gmail.com>
2017-08-15 00:13:39 +0300

    setup script classifiers ADD

--- setup.py
+++ setup.py
@@ -9,15 +9,21 @@ import os
 local_path = os.path.dirname(__file__)
 
 with open('readme.rst') as _f:
-    description = _f.read()
+    long_description = _f.read()
 
 setup(
     name='logging_tools',
     version='0.0.1',
     author='Sergey Punkoff',
-    url='',
-    description=description,
     author_email='svpmailbox@gmail.com',
+    url='https://github.com/sergyp/logging_tools',
+    download_url='https://github.com/sergyp/logging_tools/tarball/master'
+    description='Simple cover of `logging` package to make configuring easy',
+    long_description=long_description,
+    classifiers=[
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Logging',
+    ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
